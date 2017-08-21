4502f512a366dce6f2d14c37debae810acd828de
CLIN <linchenyin@gmail.com>
2017-08-20 18:41:39 -0700

    Add passing info

--- index.php
+++ index.php
@@ -13,19 +13,7 @@ printHeader();
 <div id="content">
     
     <div id="box1">
-      
-      <h3>Our Mission 任務</h3>
-      <p>To be the trusted leader in quality health care that is personalized, compassionate, and innovative. We stand for empowerment, integrity, respect, allegiality, and vitality. We commit to authenticity and excellence in clinical health care. You can count on us to be fully present in the delivery of uncompromising health care.</p>
-
-
-       <!--<img src="images/articlenews.720.20140525.png" width="730px" alt="news">-->
-      <!--<p><img src="images/article2new.jpg" width="705" height="313" alt="news2"></p>-->
-      <!--<p><img src="images/case studies.png" width="735" height="3866" alt="casestudy"><br class="clear" />
-      </p>-->
-      
-      <h3>About 關於</h3>
-      <p>Oeyama-Moto Cancer Research Foundation was formed by the late Dr. Wada Tai Hei and his sons, now led by Dr. Benedict Liao also known as Dr. Wada Masao II. We specialize in every field of medicine, including surgical oncology, urology, gynecologic oncology and radiation oncology.</p>
-      <?php /* removed as 5/6/2017
+      <?php /* removed as 5/6/2017 
       <h3>FDA Submission 食品藥品管理局審查</h3>
       <div id="fdaLetters">
         <p><b>2014年5月9日<br/>美國食品藥品監督管理局 經審查我們的提交<br/>並決定第二期臨床實驗可以開始</b>
@@ -37,6 +25,22 @@ printHeader();
         <img  class="fdaImage" src="/images/clinicaltrials.jpg">
       </div>
       */?>
+      <h3 style="color:red;">FDA Submission 食品藥品管理局審查</h3>
+      <p style="color:red;">2015年1月24曰通過完成美國藥物管理局FDA的第二階段的人體試驗<br/>
+      Passed US Food and Drug Administration (FDA) Phase II human testing on January 24th, 2015.
+      </p>
+      <h3>Our Mission 任務</h3>
+      <p>To be the trusted leader in quality health care that is personalized, compassionate, and innovative. We stand for empowerment, integrity, respect, allegiality, and vitality. We commit to authenticity and excellence in clinical health care. You can count on us to be fully present in the delivery of uncompromising health care.</p>
+
+
+       <!--<img src="images/articlenews.720.20140525.png" width="730px" alt="news">-->
+      <!--<p><img src="images/article2new.jpg" width="705" height="313" alt="news2"></p>-->
+      <!--<p><img src="images/case studies.png" width="735" height="3866" alt="casestudy"><br class="clear" />
+      </p>-->
+      
+      <h3>About 關於</h3>
+      <p>Oeyama-Moto Cancer Research Foundation was formed by the late Dr. Wada Tai Hei and his sons, now led by Dr. Benedict Liao also known as Dr. Wada Masao II. We specialize in every field of medicine, including surgical oncology, urology, gynecologic oncology and radiation oncology.</p>
+      
       <h3>Dr. Benedict Liao's experience &amp; training</h3>
       <ul>
           <li>Gynecology Oncology Fellow, <br/>Temple University Hospital</li>               
